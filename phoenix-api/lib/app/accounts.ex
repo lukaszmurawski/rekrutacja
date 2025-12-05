7c445b5ea3ce1237ef847d35d6e10bb890608084
Łukasz Murawski <l.murawski@yahoo.com>
2025-12-05 17:03:08 +0100

    phoenix - fix repo

--- phoenix-api/lib/app/accounts.ex
+++ phoenix-api/lib/app/accounts.ex
@@ -85,6 +85,7 @@ defmodule App.Accounts do
   # GET /users/:id
   # -------------------------
   def get_user!(id), do: Repo.get!(User, id)
+  def get_user(id), do: Repo.get(User, id)
 
   # -------------------------
   # POST /users
